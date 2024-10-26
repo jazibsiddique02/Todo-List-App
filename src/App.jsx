a4149533e56447ccd68f8ab6837f2fb81df91d67
jazibsiddique02 <rajajazibsiddique@gmail.com>
2024-10-26 08:25:40 +0500

    Component code

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,18 @@
+import './App.css';
+import AddTodo from "./Components/AddTodo";
+import AppName from "./Components/AppName";
+import TodoItem1 from "./Components/TodoItem1";
+import TodoItem2 from "./Components/TodoItem2";
+
+
+
 function App() {
   return <center class='todo-container'>
-    <h1>TODO App</h1>
-    <div class="container text-center">
-      <div class='row'>
-        <div class="col-6">
-          <input type="text" placeholder="Enter Todo Here" />
-        </div>
-        <div class="col-4">
-          <input type="date" />
-        </div>
-        <div class="col-2">
-          <button class='btn btn-success w-50'>Add</button>
-        </div>
-      </div>
-
-      <div class='row'>
-        <div class="col-6">
-          Buy Milk
-        </div>
-        <div class="col-4">
-          4/10/2023
-        </div>
-        <div class="col-2">
-          <button class='btn btn-danger w-50'>Delete</button>
-        </div>
-      </div>
-
-
-      <div class='row'>
-        <div class="col-6">
-          Play Tennis
-        </div>
-        <div class="col-4">
-          5/10/2023
-        </div>
-        <div class="col-2">
-          <button class='btn btn-danger w-50'>Delete</button>
-        </div>
-      </div>
-
-    </div>
+    <AppName />
+    <AddTodo />
+    <TodoItem1 />
+    <TodoItem2 />
   </center>
-
-
-
 }
 
-export default App
+export default App;
